609c9e208fa239f2a68f42404c47648fa0182580
Nahvin <55419300+Nahvin00@users.noreply.github.com>
2019-12-03 23:04:32 +0800

    Update One.cpp

--- One.cpp
+++ One.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int nume1,nume2,nume3,numf1,numf2,num1,num2,num3;
+    int numf1,numf2,num1,num2,num3,mink,minj,diff,min=2000;;
+    cout<<"Enter two integers: \n";
     cin>>numf1>>numf2;
     num1=numf1;
-    nume1=numf1;
     num2=numf2;
-    nume2=numf2;
     if(num1>num2){
         while(num2!=0){
             num3=num2;
@@ -17,4 +15,17 @@ int main()
             num1=num3;
         }
     }
-    cout<<num3;
+    else if(num1<num2){
+        while(num1!=0){
+            num3=num1;
+            num1=(num2%num3);
+            num2=num3;
+        }
+    }
+    else{
+        cout<<"You have entered same number.\n";
+        exit(0);
+    }
+    cout<<"\nThe GCD("<<numf1<<","<<numf2<<") = "<<num3<<endl;
+    return 0;
+}
